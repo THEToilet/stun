6431f1eaf9fcfd25dd9611c1c68ecc4b15883cba
THEToilet <bp18015@shibaura-it.ac.jp>
2021-12-23 08:39:29 +0900

    :sparkles: logを出力させるようにする

--- src/main/java/jp/ac/shibaura_it/se/minet/stun/EchoTask.java
+++ src/main/java/jp/ac/shibaura_it/se/minet/stun/EchoTask.java
@@ -4,28 +4,31 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.util.Date;
+import org.slf4j.Logger;
 
 public class EchoTask implements Runnable {
     private DatagramSocket datagramSocket;
     private InetAddress IPAddress;
     private int port;
+    private Logger logger;
 
-    public EchoTask(DatagramSocket datagramSocket, InetAddress IPAddress, int port) {
+    public EchoTask(DatagramSocket datagramSocket, InetAddress IPAddress, int port, Logger logger) {
         this.datagramSocket = datagramSocket;
         this.IPAddress = IPAddress;
         this.port = port;
+        this.logger = logger;
     }
 
     @Override
     public void run() {
         String adderPort = "{ type : STUN, ip : " + IPAddress.getHostAddress().toString() + ", port : " + port + "}";
-        System.out.printf("[%s] %s : Accepted!\n", new Date(), adderPort);
+        this.logger.debug("[%s] %s : Accepted! " + new Date().toString(), adderPort);
         try {
             datagramSocket.send(new DatagramPacket(adderPort.getBytes(),
                     adderPort.getBytes().length, IPAddress, port));
         } catch (Exception e) {
             e.printStackTrace();
         }
-        System.out.printf("[%s] %s : Task succeeded.\n", new Date(), adderPort);
+        this.logger.debug("[%s] %s : Task succeeded. " + new Date().toString(), adderPort);
     }
 }
--- src/main/java/jp/ac/shibaura_it/se/minet/stun/Main.java
+++ src/main/java/jp/ac/shibaura_it/se/minet/stun/Main.java
@@ -6,22 +6,27 @@ import java.util.Date;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
     static final int PORT_NUMBER = 55554;
     static final int MAX_THREADS = 100;
 
     public static void main(String[] args) throws IOException {
+        Logger logger = LoggerFactory.getLogger(Main.class);
+
         ExecutorService executor = Executors.newFixedThreadPool(MAX_THREADS);
 
         DatagramSocket datagramSocket = new DatagramSocket(PORT_NUMBER);
-        System.out.printf("[%s] Server running. port->%d\n", new Date(),  PORT_NUMBER);
+        logger.debug("[%s] Server running. port->%d " + new Date().toString(), PORT_NUMBER);
         while (true) {
             DatagramPacket receivePacket = new DatagramPacket(new byte[1024], 1024);
             try {
                 datagramSocket.receive(receivePacket);
                 InetAddress IPAddress = receivePacket.getAddress();
                 int port = receivePacket.getPort();
-                executor.submit(new EchoTask(datagramSocket, IPAddress, port));
+                executor.submit(new EchoTask(datagramSocket, IPAddress, port, logger));
             } catch (IOException e) {
                 e.printStackTrace();
                 break;
